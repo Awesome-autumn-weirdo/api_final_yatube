e5bc38ac30687bbcf09ceb3e79d143b59cb28c50
Валерия Качигина <vfvjxrf0508@outlook.com>
2025-03-22 15:58:14 +0700

    пепа

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -6,7 +6,9 @@ from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
 
 User = get_user_model()
 
+
 class UserCreateSerializer(BaseUserCreateSerializer):
+
     class Meta(BaseUserCreateSerializer.Meta):
         model = User
         fields = ['id', 'username', 'password', 'email']
